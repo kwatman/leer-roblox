e996be7cbc9b6faa446b39fb659730df47965448
kwatman <yente.paternotte@hotmail.com>
2022-07-02 14:52:17 +0200

    added google verification tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <meta name="google-site-verification" content="aM4ZGl8sQB5xydE2VA3uaUGukqpyXgzgLWsn8O3ZUKc" />
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
